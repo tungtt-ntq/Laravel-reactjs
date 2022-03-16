edfa747b21437a0d3f481d01d33c13303fa5093f
trinh.tung <tung.trinh@ntq-solution.com.vn>
2022-03-16 10:38:31 +0700

    init

---
+++ resources/js/routes/Public.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route } from 'react-router';
+import Base from '../Base';
+
+const PublicRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (
+      <Base>
+        <Component {...props} />
+      </Base>
+    )}
+  />
+);
+
+PublicRoute.propTypes = {};
+
+export default PublicRoute;
